8ce8c769dcf1c4d4ac12c3a5099076f3860b43c1
Behnam Rahmati <behnam.main@gmail.com>
2025-03-03 15:53:23 +0330

    nextAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,17 @@
-
 import NextAuth from "next-auth";
-import { authOptions } from "./_Authoptions";
+import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "@/libs/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-
-
-
-const handler = NextAuth(authOptions);
+const handler = NextAuth({
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID!, // Set in your .env file
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!, // Set in your .env file
+		}),
+		// Add more providers here (e.g. GitHubProvider, FacebookProvider, etc.)
+	],
+	secret: process.env.NEXTAUTH_SECRET, // Generate using 'openssl rand -base64 32'
+});
 export { handler as GET, handler as POST };
